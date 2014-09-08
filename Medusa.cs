49f2ecd9683bae80c7559033fcb9a18a0d6963a3
lucaslizama <lucaslizama3@hotmail.com>
2014-09-07 23:27:58 -0300

    Agregado intento de parpadeo a la medusa
    Este cambio esta sujeto a reconsideracion.

--- Medusa.cs
+++ Medusa.cs
@@ -3,93 +3,102 @@ using System.Collections;
 
 public class Medusa : MonoBehaviour
 {
-		public int hp;
-		public float medusaSpeed;
-		public int behaveIDX;
-		public bool sleep;
-		public GameObject[] bloodSpawns;
-		private	Animator anim;
-		private BoxCollider2D caja;
-		private CircleCollider2D circulo;
-		private int spawnNumber;
+    public int hp;
+    public float medusaSpeed;
+    public int behaveIDX;
+    public GameObject[] bloodSpawns;
+    private Animator anim;
+    private BoxCollider2D caja;
+    private CircleCollider2D circulo;
+    private Color medusaColor;
+    private int spawnNumber;
 
 
-		// Use this for initialization
-		void Start ()
-		{
-				sleep = false;
-				anim = GetComponent<Animator> ();
-				caja = GetComponentInChildren<BoxCollider2D> ();
-				circulo = GetComponentInChildren<CircleCollider2D> ();
-		}
-	
-		void FixedUpdate ()
-		{
-				if (sleep == true && behaveIDX == 0) {
-						dormirObjeto ();
-				}
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        caja = GetComponentInChildren<BoxCollider2D>();
+        circulo = GetComponentInChildren<CircleCollider2D>();
+        medusaColor = GetComponent<SpriteRenderer>().color;
+    }
 
-				if (hp <= 0) {
-						morir ();
-				}
+    void FixedUpdate()
+    {
+        if (hp <= 0)
+        {
+            morir();
+        }
 
-				if (behaveIDX == 1) {
-						moveLeft (medusaSpeed);
-				} else if (behaveIDX == 2) {
-						moveRight (medusaSpeed);
-				}
-		}
-
-		void OnCollisionEnter2D (Collision2D objeto)
-		{
-				if (objeto.gameObject.tag == "Bomb") {
-						if (objeto.gameObject.name == "Grenade(Clone)") {
-								print ("Impacto");
-								Grenade gr = objeto.gameObject.GetComponent<Grenade> ();
-								gr.explode = true;
-						}
-				}
-
-				if (objeto.gameObject.tag == "bullet") {
-						if (objeto.gameObject.name == "bullet(Clone)") {
-								hp = hp - 1;
-								bullet bala = objeto.gameObject.GetComponent<bullet> ();
-								bala.hit = true;
-								if (hp > 0) {
-										spawnNumber = Random.Range (0, 4);
-										bloodSpawns [spawnNumber].SetActive (true);
-										spawnNumber = spawnNumber + 1;
-								}
-						}
-				}
+        if (behaveIDX == 1)
+        {
+            StartCoroutine("flash");
+            behaveIDX = 0;
+        }
 
 
-		}
+    }
 
-		void moveLeft (float medusaSpeed)
-		{
-				transform.Translate (new Vector3 (-(this.medusaSpeed), 0f, 0f));
-		}
+    void OnCollisionEnter2D(Collision2D objeto)
+    {
+        if (objeto.gameObject.tag == "Bomb")
+        {
+            if (objeto.gameObject.name == "Grenade(Clone)")
+            {
+                print("Impacto");
+                Grenade gr = objeto.gameObject.GetComponent<Grenade>();
+                gr.explode = true;
+            }
+        }
 
-		void moveRight (float medusaSpeed)
-		{
-				transform.Translate (new Vector3 (this.medusaSpeed, 0f, 0f));
-		}
+        if (objeto.gameObject.tag == "bullet")
+        {
+            if (objeto.gameObject.name == "bullet(Clone)")
+            {
+                behaveIDX = 1;
+                hp = hp - 1;
+                bullet bala = objeto.gameObject.GetComponent<bullet>();
+                bala.hit = true;
+                if (hp > 0)
+                {
+                    spawnNumber = Random.Range(0, 4);
+                    bloodSpawns[spawnNumber].SetActive(true);
+                    spawnNumber = spawnNumber + 1;
+                }
+            }
+        }
 
-		public void morir ()
-		{
-				caja.enabled = false;
-				circulo.enabled = false;
-				anim.SetTrigger ("Death");
-		}
 
-		void destruir ()
-		{
-				Destroy (gameObject);
-		}
-			
-		void dormirObjeto ()
-		{
-				rigidbody2D.Sleep ();
-		}
+    }
+
+    public IEnumerator flash()
+    {
+        //medusaColor.a = 0.78f;
+        medusaColor.b = 0f;
+        medusaColor.g = 0f;
+        GetComponent<SpriteRenderer>().color = medusaColor;
+        yield return new WaitForSeconds(0.1f);
+        //medusaColor.a = 1f;
+        medusaColor.b = 1f;
+        medusaColor.g = 1f;
+        GetComponent<SpriteRenderer>().color = medusaColor;
+        StopCoroutine("flash");
+    }
+
+    public void morir()
+    {
+        caja.enabled = false;
+        circulo.enabled = false;
+        anim.SetTrigger("Death");
+    }
+
+    void destruir()
+    {
+        Destroy(gameObject);
+    }
+
+    void dormirObjeto()
+    {
+        rigidbody2D.Sleep();
+    }
 }

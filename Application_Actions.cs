b7a06160051a675c1a6f23086ac367d22ec49d33
lucaslizama <lucaslizama3@hotmail.com>
2014-09-24 00:37:01 -0300

    afsdfgsdfgsd
    sdfgsdfgdfsg

---
+++ Application_Actions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Application_Actions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void cargaNivel(int level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void cierraJuego()
+    {
+        Application.Quit();
+    }
+}
